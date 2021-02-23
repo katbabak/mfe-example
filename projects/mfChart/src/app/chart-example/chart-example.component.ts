760c4c59308af802fb1ee87448373b5aa4c0d5fc
katebabak <kateryna.babak@windmill.ch>
2021-02-23 11:09:30 +0200

    add chart

--- projects/mfChart/src/app/chart-example/chart-example.component.ts
+++ projects/mfChart/src/app/chart-example/chart-example.component.ts
@@ -1,14 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import * as Highcharts from 'highcharts';
+import * as Exporting from 'highcharts/modules/exporting';
+import {Options} from "highcharts";
+
+// Exporting(Highcharts);
 
 @Component({
   selector: 'app-chart-example',
   templateUrl: './chart-example.component.html'
 })
-export class ChartExampleComponent implements OnInit {
+export class ChartExampleComponent implements AfterViewInit {
+  @ViewChild('chartContainer') public container: ElementRef;
 
+  public options: any = {
+    chart: {
+      type: 'scatter',
+      height: 700
+    },
+    title: {
+      text: 'Sample Scatter Plot'
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      formatter: function() {
+        return 'x: ' + Highcharts.dateFormat('%e %b %y %H:%M:%S', this.x) +
+          'y: ' + this.y.toFixed(2);
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      labels: {
+        formatter: function() {
+          return Highcharts.dateFormat('%e %b %y', this.value);
+        }
+      }
+    },
+    series: [
+      {
+        name: 'Normal',
+        turboThreshold: 500000,
+        data: []
+      },
+      {
+        name: 'Abnormal',
+        turboThreshold: 500000,
+        data: []
+      }
+    ]
+  };
+  public optVersion2: any = {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten'
+      }
+    },
+    series: [{
+      name: 'Jane',
+      data: [1, 0, 4]
+    }, {
+      name: 'John',
+      data: [5, 7, 3]
+    }],
+  };
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    console.log(this.container);
+    Highcharts.chart(this.container.nativeElement, this.optVersion2)
   }
-
 }
--- projects/mfChart/webpack.config.js
+++ projects/mfChart/webpack.config.js
@@ -36,6 +36,7 @@ module.exports = {
           "@angular/core": { singleton: true, strictVersion: true },
           "@angular/common": { singleton: true, strictVersion: true },
           "@angular/router": { singleton: true, strictVersion: true },
+          "highcharts": { singleton: true, strictVersion: true },
 
           ...sharedMappings.getDescriptors()
         }
